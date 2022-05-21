9c8cd79dfa01010219d4655ee97346a003a9aec9
romanovictoria <vicctoriarom@gmail.com>
2022-05-21 14:22:37 -0300

    se  avanzo con el javascript en la seccion calculadora unificando ambas secciones

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,52 @@
-function() {
-
+function CalculadoraAvanzada() {
+    const listaElementosEscondidos = document.getElementsByClassName('esconder');
+    for (let item of listaElementosEscondidos) {
+        item.classList.remove('esconder');
+        item.classList.add('mostrar');
+    }
+    const botonAvanzada = document.getElementById('calculadora-avanzada')
+    botonAvanzada.classList.add('esconder')
+    const botonSimple = document.getElementById('calculadora-simple')
+    botonSimple.classList.remove('esconder')
 }
+
+/**
+ * CalculadoraSimple();
+ * @helper
+ * sirve para blabalbalbla
+ */
+function CalculadoraSimple() {
+    const listaElementosEscondidos = document.getElementsByClassName('mostrar');
+    for (let item of listaElementosEscondidos) {
+        item.classList.remove('mostrar');
+        item.classList.add('esconder');
+    }
+    const botonAvanzada = document.getElementById('calculadora-avanzada')
+    botonAvanzada.classList.remove('esconder')
+    const botonSimple = document.getElementById('calculadora-simple')
+    botonSimple.classList.add('esconder')
+}
+
+/**
+ * calcularROI()
+ * @param invInicial sirve para blabalbalba
+ * @param ganancia
+ * @param invTotal
+ * @returns {number}
+ */
+function calcularROI(invInicial, ganancia, invTotal) {
+    const roi = (ganancia - invInicial)/invTotal;
+    return roi;
+}
+
+function calcular() {
+    // ... implementacion
+    // llamar calcularROI(valores....)
+}
+
+// traer un elemento por id
+// const botonSimple = document.getElementById('calculadora-simple')
+// const valor = botonSimple.target.value
+// console.log(valor)
+// const resultado = calcularROI(valores...)
+// console.log(resultado) ---> lo visualizamos en el inspector
