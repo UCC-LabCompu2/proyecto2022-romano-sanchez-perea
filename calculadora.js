967556d357ccab8c1cda9a5f4b80175a04704cf7
romanovictoria <vicctoriarom@gmail.com>
2022-06-04 16:47:19 -0300

    se avanzo con el proyecto

--- calculadora.js
+++ calculadora.js
@@ -39,14 +39,51 @@ function calcularROI(invInicial, ganancia, invTotal) {
     return roi;
 }
 
+function cargarResultado{
+    const urlcomp = window.location.href; // es una funcion traer url del navegador
+    console.log(urlcomp) //  console.log imprimir la url
+    // obterner el div por ID
+    // const divResultado = getElm(id)
+    // obtener inicial, ganancia, total
+    // const inicial = getElm(inputInicial) => repetir con ganancia y total
+    /*
+        HTMLElement Input
+        console.log(Input)
+        // usuario hizo => document.getElementById("Gasto_Usuario").value = 445;
+        // nosotro hacemos => const valueInicial = document.getElementById("Gasto_Usuario").value;
+
+        const valueInicial
+        const ganancia
+        const total
+    */
+    // obtener ROI
+    // const valor = calcularROI(inicial, ganancia, total);
+    // ponerle el resultado al div
+    // innerHTML = `El resultado... ${valor}
+}
+
+alert ("se ingreso un valor invalido, por favor ingrese de nuevo");
 function calcular() {
     // ... implementacion
     // llamar calcularROI(valores....)
 }
 
+/**
+ * const lista = [a,b,c,d,e]
+ * const value = "abc" (char*) // abc....null|0|nil
+ *
+ * lista[0] => a
+ * lista[2] => c
+ * lista[34] => out of index
+ *
+ * value[0] => a
+ * value[3] => error || undefined
+ * getkjaskldjfklkjkl()[2]
+ */
+
 // traer un elemento por id
 // const botonSimple = document.getElementById('calculadora-simple')
-// const valor = botole.target.nSimpvalue
+// const valor = boton.target.nSimpvalue
 // console.log(valor)
 // const resultado = calcularROI(valores...)
 // console.log(resultado) ---> lo visualizamos en el inspector
